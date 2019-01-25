a31c3c2a16fe3bb0f682f1374a99f82b5faa3fe5
Luca Regnicoli <lucaregnicoli@gmail.com>
2019-01-25 16:19:49 +0000

    Desktop.Client and FunctionalTests

---
+++ Consumers/Desktop/Desktop.Client/Controllers/ShoppingController.cs
@@ -0,0 +1,45 @@
+﻿using Desktop.Client.Models;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Desktop.Client
+{
+    public class ShoppingController
+    {
+        //TODO: Add a configuration enum instead of address - for example: Staging/Production
+        public Uri Url { get; set; } = new Uri("http://localhost:5201");
+
+        public async Task AddBasketItemAsync(AddBasketItemRequest data, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var aggregator = new ShoppingAggregator(Url, null);
+
+            await aggregator.ApiV1ShoppingItemsPostWithHttpMessagesAsync(data, null, cancellationToken);
+        }
+
+        public async Task<BasketData> UpdateQuantitiesAsync(UpdateBasketItemsRequest data, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var aggregator = new ShoppingAggregator(Url, null);
+
+            var response = await aggregator.ApiV1ShoppingItemsPutWithHttpMessagesAsync(data, null, cancellationToken);
+
+            return response.Body;
+        }
+
+        public async Task<BasketData> UpdateAllBasketAsync(UpdateBasketRequest data, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var aggregator = new ShoppingAggregator(Url, null);
+
+            var response = await aggregator.ApiV1ShoppingPostWithHttpMessagesAsync(data, null , cancellationToken);
+
+            return response.Body;
+        }
+
+        public async Task DeleteBasketAsync(string id, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var aggregator = new ShoppingAggregator(Url, null);
+
+            await aggregator.ApiV1ShoppingByIdDeleteWithHttpMessagesAsync(id);
+        }
+    }
+}
---
+++ Consumers/Desktop/Desktop.FunctionalTests/ShoppingScenarios.cs
@@ -0,0 +1,35 @@
+﻿using Desktop.Client;
+using Desktop.Client.Models;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Desktop.FunctionalTests
+{
+    public class ShoppingScenarios
+    {
+        public Uri Url { get; set; } = new Uri("http://localhost:5201");
+
+        [Fact]
+        public async Task AddBasketItemAsync_Returns_Ok()
+        {
+            // Arrange
+            var data = new AddBasketItemRequest()
+            {
+                BasketId = "Customer00",
+                CatalogItemId = 10,
+                Quantity = 5
+            };
+            var controller = new ShoppingController()
+            {
+                Url = Url
+            };
+
+            // Act
+            await controller.AddBasketItemAsync(data);
+
+            // Assert - Just check that an exception has not been thrown
+        }
+
+    }
+}
